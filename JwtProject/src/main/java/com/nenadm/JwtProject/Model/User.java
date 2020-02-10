5363c4c2c686a77e3670d3727401cb87b6b8c791
nenadmit <58050465+nenadmit@users.noreply.github.com>
2020-02-10 02:24:03 +0100

    Update User.java

--- JwtProject/src/main/java/com/nenadm/JwtProject/Model/User.java
+++ JwtProject/src/main/java/com/nenadm/JwtProject/Model/User.java
@@ -1,8 +1,6 @@
 package com.nenadm.JwtProject.Model;
 
-import java.io.Serializable;
-
-public class User implements  Serializable {
+public class User {
 
     private static final long serialVersionUID = 1L;
 
